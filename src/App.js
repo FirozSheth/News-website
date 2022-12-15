ba30c06f7f5798400a5a41ab64885aeddea54086
Firozsheth <shethfiroz18@gmail.com>
2022-12-15 22:06:08 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -17,29 +17,29 @@ export default class App extends Component {
     progress:0
   }
 
-  setProgress=(progress)=>{
+  setProgress = (progress)=>{
     this.setState({progress: progress})
   }
+
   render() {
     return (
       <div>
         <Router>
-        <NavBar/>
+        <NavBar/> 
         <LoadingBar
-        height={4}
+        height={3}
         color='#f11946'
-        progress={this.state.progress}
+        progress={this.state.progress} 
       />
- 
         <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/business"><News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
-          <Route exact path="/entertainment"><News setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
-          <Route exact path="/general"><News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/health"><News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
-          <Route exact path="/science"><News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
-          <Route exact path="/sports"><News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
-          <Route exact path="/technology"><News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
+          <Route exact path="/"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
         </Switch>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,29 +1,26 @@
-import { getByDisplayValue } from '@testing-library/react'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-
-
     render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
-                    <div style={{display: 'flex',
-                    justifyContent: 'flex-end',
-                    position: 'absolute',
-                    right:'0'}}>
-                        
-                    <span className="badge rounded-pill bg-danger">
-                    {source}
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
 
-                </span>
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
                     </div>
-                    <img src={!imageUrl ? "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/untitled-design-1670383855.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" : imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
